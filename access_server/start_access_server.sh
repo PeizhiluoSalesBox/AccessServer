12ac95a88f014cde09a1d26fbfb7e5baf7c15641
13758193961 <13758193961@126.com>
2018-11-09 13:56:04 +0800

    add accessserverip in env

--- access_server/start_access_server.sh
+++ access_server/start_access_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #从环境变量中读取等信息
 #
+#
 echo "access_server is Start!"
-./access_server -u 8000 -a $RedisIP -p $RedisPort
+./access_server -i $ServerIP -u 8000 -a $RedisIP -p $RedisPort
 echo "access_server is Exit!"
